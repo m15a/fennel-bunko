33947474cb55343cd133387d33f9c883b5078bdc
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-02-16 19:48:06 +0900

    chore(tools): use fennel current head

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    fennel = {
+      url = "sourcehut:~technomancy/fennel/main";
+      flake = false;
+    };
     fenneldoc = {
       url = "git+https://github.com/Olical/fenneldoc?submodules=1";
       flake = false;
@@ -11,6 +15,24 @@
   outputs = { self, nixpkgs, flake-utils, ... } @ inputs:
   let
     overlay = final: prev: {
+      fennel = final.stdenv.mkDerivation rec {
+        pname = "fennel";
+        version = "1.4.1-dev";
+        src = inputs.fennel;
+        nativeBuildInputs = [
+          final.lua5_3
+          final.pandoc
+        ];
+        postPatch = ''
+          # FIXME: maninst function and run ./fennel do not work.
+          sed -i Makefile \
+              -e 's|$(call maninst,$(doc),$(DESTDIR)$(MAN_DIR)/$(doc))|$(shell mkdir -p $(dir $(DESTDIR)$(MAN_DIR)$(doc)) && cp $(doc) $(DESTDIR)$(MAN_DIR)/$(doc))|' \
+              -e 's|\./fennel|lua ./fennel|'
+        '';
+        makeFlags = [
+          "PREFIX=$(out)"
+        ];
+      };
       fenneldoc = final.stdenv.mkDerivation rec {
         pname = "fenneldoc";
         version = "1.0.1";
@@ -39,9 +61,9 @@
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            fenneldoc
-          ] ++ (with lua.pkgs; [
             fennel
+            fenneldoc
+          ] ++ (with lua5_3.pkgs; [
             readline
           ]);
         };
